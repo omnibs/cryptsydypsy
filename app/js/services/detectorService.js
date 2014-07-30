d980594b45340312b485fd9883486ddaffcc0193
Leonardo Stern <leonardo.stern@gmail.com>
2014-07-29 22:15:25 -0300

    adicionando mais indicadores

--- app/js/services/detectorService.js
+++ app/js/services/detectorService.js
@@ -1,7 +1,7 @@
 app.service('detectorService', ['notifyService',function (notifyService) {
 	var state = {
 		maxLength: 100000,
-		timelength: 60*5, //5min
+		timelength: 60, //5min
 		overprovision: 60, // +60s de overprovision pra contar periodos de inatividade e nao parar de analisar durante eles
 		buffer: [], 
 		Sell: {volume: 0, delta: 0, lastPrice: null},
@@ -10,11 +10,22 @@ app.service('detectorService', ['notifyService',function (notifyService) {
 		lastPrice: null,
 		priceDelta: null
 	};
-	
-	var oldState = state;
+
+	var oldStates = [state];
+	var nStates = 0;
+
+	var consolidate = function(data){
+		var result = { sellVolume: 0, buyVolume: 0, minPrice: 99999, maxPrice: 0 };
+		for (var i = 0; i < data.length; i++) {
+			result.sellVolume += data[i].Sell.volume;
+			result.buyVolume += data[i].Buy.volume;
+		}
+
+		return result;
+	};
 
 	var svc = {
-		push: function(data){
+   		push: function(data){
 			state.buffer.push(data.trade);
 
 			var overflown = state.buffer.length - state.maxLength;
@@ -47,8 +58,8 @@ app.service('detectorService', ['notifyService',function (notifyService) {
 				priceMin = Math.min(priceMin, price);
 			};
 
-			state.Buy.delta = (state.Buy.volume / oldState.Buy.volume)-1;
-			state.Sell.delta = (state.Sell.volume / oldState.Sell.volume)-1;
+			state.Buy.delta = (state.Buy.volume / oldStates[nStates].Buy.volume)-1;
+			state.Sell.delta = (state.Sell.volume / oldStates[nStates].Sell.volume)-1;
 			state.lastTrade = this.lastTrade();
 			state.lastPrice = state.lastTrade.price;
 			state.maxPrice = priceMax;
@@ -67,7 +78,7 @@ app.service('detectorService', ['notifyService',function (notifyService) {
 			}
 			// se aumentou mais de 10% volume em 10 segundos == pump/dump? axo q nao neh, mas vai por enquanto
 			var delta = state.Buy.delta;
-			var abs = state.Buy.volume - oldState.Buy.volume;
+			var abs = state.Buy.volume - oldStates[nStates].Buy.volume;
 			
 			if (false && delta > 0.1) // mto falso positivo
 				notifyService.notify('Pump?', 'Delta de volume em 10s \r\n' 
@@ -80,7 +91,7 @@ app.service('detectorService', ['notifyService',function (notifyService) {
 			
 
 			delta = state.Sell.delta;
-			abs = state.Buy.volume - oldState.Buy.volume;
+			abs = state.Buy.volume - oldStates[nStates].Buy.volume;
 			if (false && delta > 0.1) // mto falso positivo
 				notifyService.notify('Dump?', 'Delta de volume em 10s \r\n' 
 					+ (delta*100).toFixed(2) + '%\r\n'
@@ -91,7 +102,7 @@ app.service('detectorService', ['notifyService',function (notifyService) {
 					+ abs.toFixed(8) + ' BTC');
 
 			delta = state.priceDelta;
-			var deltaChange = (delta / oldState.priceDelta)-1;
+			var deltaChange = (delta / oldStates[nStates].priceDelta)-1;
 			abs = state.maxPrice - state.minPrice;
 			if (delta >= 0.05 && delta != Infinity && deltaChange > 0.01)
 				notifyService.notify('Pump?', 'Delta de preço em 5min aumentou \r\n' 
@@ -104,9 +115,24 @@ app.service('detectorService', ['notifyService',function (notifyService) {
 					+ (-abs).toFixed(8) + ' BTC');
 		},
 		getState: function() {
+			var last5 = consolidate(oldStates.concat().splice(-5,5));
+			var previous5 = consolidate(oldStates.concat().splice(-10,5));
+			var last10 = consolidate(oldStates.concat().splice(-10,10));
+			var previous10 = consolidate(oldStates.concat().splice(-20,10));
+
+			//{volume: 0, delta: 0, lastPrice: null}
+			var sell5m = { volume: last5.sellVolume, delta: last5.sellVolume - previous5.sellVolume, lastPrice: state.Sell.lastPrice};
+			var sell10m = { volume: last10.sellVolume, delta: last10.sellVolume - previous10.sellVolume, lastPrice: state.Sell.lastPrice};
+			var buy5m = { volume: last5.buyVolume, delta: last5.buyVolume - previous5.buyVolume, lastPrice: state.Buy.lastPrice};
+			var buy10m = { volume: last10.buyVolume, delta: last10.buyVolume - previous10.buyVolume, lastPrice: state.Buy.lastPrice};
+
 			return {
-				sell: state.Sell, 
-				buy: state.Buy,
+				sell1m: state.Sell, 
+				sell5m: sell5m, 
+				sell10m: sell10m, 
+				buy1m: state.Buy,
+				buy5m: buy5m,
+				buy10m: buy10m,
 				lastTrade: state.lastTrade, 
 				lastPrice: state.lastPrice, 
 				priceDelta: state.priceDelta,
@@ -124,8 +150,12 @@ app.service('detectorService', ['notifyService',function (notifyService) {
 	};
 
 	setInterval(function() {
-		oldState = JSON.parse(JSON.stringify(state));
+		oldStates.push(JSON.parse(JSON.stringify(state)));
+		if (oldStates.length > 20){
+			oldStates.splice(1);
+		}
 
+		nStates = oldStates.length - 1;
 		//svc.removeOlder();
 		//svc.recalc();
 		svc.check();
